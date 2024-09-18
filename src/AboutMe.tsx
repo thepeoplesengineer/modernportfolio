64341bc8e015b7b693ea521b86c6de04664b1bfd
Sammy Kordi <sammy.kordi@gmail.com>
2024-09-17 18:06:41 -0700

    CSS styling touch-ups

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -12,9 +12,9 @@ const AboutMe: React.FC = () => {
           <h1>Let's Build Together.</h1>
           <h3>Full Stack – Programmer</h3>
           <p>
-            I'm Sammy Kordi, a tech enthusiast with a strong foundation in leadership and business management. After spending several years leading teams, driving strategies, and managing high-impact projects in the business world, I’ve decided to pivot and immerse myself in the exciting world of tech development.
+            I'm Sammy Kordi, a tech enthusiast with a strong foundation in leadership and business management. After spending 15 years leading teams, driving strategies, and managing high-impact projects in the business world, I’ve decided to pivot and immerse myself in the exciting world of tech development.
 
-Why the switch? I’ve always believed in the power of technology to transform businesses and improve lives. As I led teams and shaped organizational strategies, I found myself increasingly drawn to the tech side of things—innovating, solving problems, and making processes more efficient. This passion pushed me to pursue a career in tech, where I can merge my leadership experience with a hands-on approach to creating solutions.
+Why the switch? I’ve always believed in the power of technology to increase productivity and improve lives. As I led teams and shaped organizational strategies, I found myself increasingly drawn to the tech side of things—innovating, solving problems, and making processes more efficient. This passion pushed me to pursue a career in tech, where I can merge my leadership experience with a hands-on approach to creating solutions.
 
 Now, as a developer, I’m focused on building sleek, functional applications that solve real-world problems. With a keen eye for detail and a drive for continuous improvement, I aim to bring the same level of leadership, accountability, and innovation to my work in tech as I did in business.
 
